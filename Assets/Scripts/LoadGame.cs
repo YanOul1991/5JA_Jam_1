f93d1d90565a43b64309b6d220332af892ceef5f
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2025-10-01 00:24:45 -0400

    Remove SceneAsset dependency in LoadGame
    Replaced the use of a serialized SceneAsset with a hardcoded scene name in LoadGame.cs. This simplifies scene loading and removes the need for UnityEditor references.

--- Assets/Scripts/LoadGame.cs
+++ Assets/Scripts/LoadGame.cs
@@ -1,15 +1,13 @@
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/* Script pour charger la scène du jeu ce qui permettra d'initialiser le serveur
+/* 
+    Script pour charger la scène du jeu ce qui permettra d'initialiser le serveur
 */
 public class LoadGame : MonoBehaviour
 {
-    [field: SerializeField] private SceneAsset m_sceneToLoad;
-
     void Start()
     {
-        SceneManager.LoadScene(m_sceneToLoad.name);
+        SceneManager.LoadScene("JeuMultiBrickJam");
     }
 }
